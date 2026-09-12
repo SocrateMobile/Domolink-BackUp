b24ae29789d2bd9286b16c1f3463f32d6a4bd2b3
socrate <Torjku11APL&>
2026-09-12 13:50:27 +0200

    fix(hacs): remove strict filename requirement and bump version to v1.5.4

--- custom_components/domolink_backup/const.py
+++ custom_components/domolink_backup/const.py
@@ -6,7 +6,7 @@ import re
 DOMAIN = "domolink_backup"
 NAME = "DomoLink-BackUp"
 DEFAULT_NAME = "DomoLink-BackUp"
-VERSION = "1.5.3"
+VERSION = "1.5.4"
 
 # Backup and Restore Types
 BACKUP_TYPE_FULL = "full"
